3a7aae58c108b8ca39f8ccfbd19e870db06f120d
Lee Ha-eun <lheun99@gmail.com>
2024-02-24 17:21:44 +0900

    240224 파일 수정 추가

---
+++ 정리/DFS+BFS/재귀함수.py
@@ -0,0 +1,10 @@
+def recurive_function(i):
+    if i == 100:
+        return
+    
+    print(i, "번째 재귀함수에서 ", i+1, '번째 재귀함수를 호출합니다.')
+    recurive_function(i+1)
+    print(i, "번째 재귀함수를 종료합니다.")
+
+
+recurive_function(1)
